dca0fbb1b64fac7da70487fafe31f64069676940
unknown <abubakarafamai@gmail.com>
2026-08-05 23:34:13 +0100

    Add Comment

--- app/Imports/ResultImport.php
+++ app/Imports/ResultImport.php
@@ -167,6 +167,7 @@ class ResultImport implements ToCollection
                             ])
                             ->where('approve', '!=', 'system')
                             ->first();
+                        dd($checking);
                         if ($checking) {
 
                         } else {
